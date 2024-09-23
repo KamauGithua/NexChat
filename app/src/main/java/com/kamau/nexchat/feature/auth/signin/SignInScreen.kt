2f1c110fb6f9389f0b9aaf7b3b1ebfeea343369f
kamau <githuakamau99@gmail.com>
2024-09-23 09:38:42 +0300

    Set up NavHost with SignIn and SignUp composables; created NavController and added navigation dependency

--- app/src/main/java/com/kamau/nexchat/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/kamau/nexchat/feature/auth/signin/SignInScreen.kt
@@ -1,2 +1,9 @@
 package com.kamau.nexchat.feature.auth.signin
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SignInScreen(navController: NavController){
+
+}
--- app/src/main/java/com/kamau/nexchat/feature/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/kamau/nexchat/feature/auth/signup/SignUpScreen.kt
@@ -1,2 +1,9 @@
 package com.kamau.nexchat.feature.auth.signup
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SignUpScreen(navController: NavController){
+
+}
